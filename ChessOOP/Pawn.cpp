f2f1332254c80a39426d855c593d976884403ab9
pkai21 <130279557+pkai21@users.noreply.github.com>
2023-05-20 23:29:01 +0700

    Update Pawn.cpp

--- ChessOOP/Pawn.cpp
+++ ChessOOP/Pawn.cpp
@@ -3,8 +3,8 @@
 
 Pawn::Pawn(char _symbol) : Piece(_symbol)
 {
-	if (symbol == 'P') img.loadFromFile("img/P.png");
-	else img.loadFromFile("img/p.png");
+	if (symbol == 'P') img.loadFromFile("img/wP.png");
+	else img.loadFromFile("img/bP.png");
 	img_size = img.getSize();
 	sprite.setTexture(img);
 }
