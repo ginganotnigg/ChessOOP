dd5c0eb1b69f0d934ff595eff3b2c01dc4621a90
pkai21 <130279557+pkai21@users.noreply.github.com>
2023-05-20 22:35:17 +0700

    Add files via upload

---
+++ ChessOOP/Pawn.cpp
@@ -0,0 +1,14 @@
+﻿#include "Pawn.h"
+
+
+Pawn::Pawn(char _symbol) : Piece(_symbol)
+{
+	if (symbol == 'P') img.loadFromFile("img/P.png");
+	else img.loadFromFile("img/p.png");
+	img_size = img.getSize();
+	sprite.setTexture(img);
+}
+
+Pawn::~Pawn()
+{
+}
---
+++ ChessOOP/Pawn.h
@@ -0,0 +1,9 @@
+#include "Piece.h"
+#include "Square.h"
+#include "Board.h"
+class Pawn : public Piece
+{
+public:
+	Pawn(char _symbol);
+	~Pawn();
+};
