8918d24ef9821d2d3021293ac3a94fff1a8c5e81
longchau006 <104685813+longchau006@users.noreply.github.com>
2023-05-21 21:43:54 +0700

    Update Bishop.cpp (modify)

--- ChessOOP/Bishop.cpp
+++ ChessOOP/Bishop.cpp
@@ -1,8 +1,8 @@
 #include "Bishop.h"
 
-Bishop::Bishop()
+Bishop::Bishop(char name): Piece(name)
 {
-	if (name = 'B') img.loadFromFile("Image/wB.png");
+	if (name == 'B') img.loadFromFile("Image/wB.png");
 	else img.loadFromFile("Image/bB.png");
 	sprite.setTexture(img);
 }
@@ -12,24 +12,22 @@ Bishop::~Bishop()
 
 }
 
-vector<Square> Bishop::getValidMoves()
+vector<Square*> Bishop::getValidMoves()
 {
-	vector<Square> squares;
+	vector<Square*> squares;
 	for (int i = 1; i <= 8; i++)
 	{
-		for (int j = int('a'); j <= int('h'); j++)
+		for (int j = 'a'; j <= 'h'; j++)
 		{
-			if ((i != position.row && j != int(position.col)) && (abs(i - position.row) == abs(j - int(position.col))))
+			if ((i != position->row && j != int(position->column)) && (abs(i - position->row) == abs(j - int(position->column))))
 			{
-				squares.push_back(Square(char(j)), i));
+				squares.push_back(new Square(j, i));
 			}
 		}
 	}
 	return squares;
 }
 
-
-void Bishop::move(const Square& pos)
-{
-
+bool Bishop::isValidMove() {
+	return 0;
 }
