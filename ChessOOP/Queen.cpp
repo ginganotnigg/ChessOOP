8af5b3967c16bad8317616767b794fb8d2c267eb
longchau006 <104685813+longchau006@users.noreply.github.com>
2023-05-21 21:49:32 +0700

    Update Queen.cpp (modify)

--- ChessOOP/Queen.cpp
+++ ChessOOP/Queen.cpp
@@ -21,14 +21,14 @@ vector<Square*> Queen::getValidMoves()
 	{
 		if (i != getCurrentPosition()->row) squares.push_back(new Square(getCurrentPosition()->column, i));
 	}
-	for (int j = int('a'); j <= int('h'); j++)
+	for (int j = 'a'; j <= 'h'; j++)
 	{
-		if (j != getCurrentPosition()->column) squares.push_back(new Square(char(j), getCurrentPosition()->row));
+		if (j != getCurrentPosition()->column) squares.push_back(new Square(j, getCurrentPosition()->row));
 	}
 	// Bishop moves
 	for (int i = 1; i <= 8; i++)
 	{
-		for (int j = int('a'); j <= int('h'); j++)
+		for (int j = 'a'; j <= 'h'; j++)
 		{
 			if ((i != getCurrentPosition()->row && j != int(getCurrentPosition()->column)) && (abs(i - getCurrentPosition()->row) == abs(j - int(getCurrentPosition()->column))))
 			{
