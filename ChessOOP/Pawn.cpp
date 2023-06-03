d6bf1a19e4b303912c11dcd320f1405815ed7c4e
pkai21 <130279557+pkai21@users.noreply.github.com>
2023-06-03 10:36:28 +0700

    Update Pawn.cpp

--- ChessOOP/Pawn.cpp
+++ ChessOOP/Pawn.cpp
@@ -19,42 +19,55 @@ vector<Square*> Pawn::getValidMoves(vector<Square*> boardSquares)
 	int row = position->row;
 	char col = position->column;
 	int dy = (name < 91) ? 1 : -1;
-	if (checkAlly(boardSquares[getSqrIdx(col, row + dy * 1)]) == -1)
+	if (checkAlly(boardSquares[getSqrIdx(col, row + dy * 1)]) == -1 &&
+		row + dy * 1 <=8 && row + dy * 1 >= 1)
 	{
 		squares.push_back(boardSquares[getSqrIdx(col, row + dy * 1)]);
 	}
 
 	if (getStatus() == 'u' &&
 		checkAlly(boardSquares[getSqrIdx(col, row + dy * 1)]) == -1 &&
-		checkAlly(boardSquares[getSqrIdx(col, row + dy * 2)]) == -1
-		)
+		row + dy * 1 <= 8 && row + dy * 1 >= 1 &&
+		checkAlly(boardSquares[getSqrIdx(col, row + dy * 2)]) == -1 &&
+		row + dy * 2 <= 8 && row + dy * 2 >= 1)
 	{
 		squares.push_back(boardSquares[getSqrIdx(col, row + dy * 2)]);
 	}
 
 	if (checkAlly(boardSquares[getSqrIdx(col - 1, row + dy * 1)]) == 0 &&
-		col - 1 >= 'a' && col - 1 <= 'h')
+		col - 1 >= 'a' && col - 1 <= 'h' &&
+		row + dy * 1 <= 8 && row + dy * 1 >= 1)
 	{
 		squares.push_back(boardSquares[getSqrIdx(col - 1, row + dy * 1)]);
 	}
 
 	if (checkAlly(boardSquares[getSqrIdx(col + 1, row + dy * 1)]) == 0 &&
-		col + 1 >= 'a' && col + 1 <= 'h')
+		col + 1 >= 'a' && col + 1 <= 'h' &&
+		row + dy * 1 <= 8 && row + dy * 1 >= 1)
 	{
 		squares.push_back(boardSquares[getSqrIdx(col + 1, row + dy * 1)]);
 	}
 
 	if (checkAlly(boardSquares[getSqrIdx(col - 1, row)]) == 0 &&
+		col - 1 >= 'a' && col - 1 <= 'h' &&
+		row + dy * 1 <= 8 && row + dy * 1 >= 1 &&
 		boardSquares[getSqrIdx(col - 1, row)]->piece->getStatus() == 'e')
 	{
 		squares.push_back(boardSquares[getSqrIdx(col - 1, row + dy * 1)]);
 	}
 
 	if (checkAlly(boardSquares[getSqrIdx(col + 1, row)]) == 0 &&
-		boardSquares[getSqrIdx(col + 1, row)]->piece->getStatus() == 'e')
+		col + 1 >= 'a' && col + 1 <= 'h' &&
+		boardSquares[getSqrIdx(col + 1, row)]->piece->getStatus() == 'e' &&
+		row + dy * 1 <= 8 && row + dy * 1 >= 1 )
 	{
 		squares.push_back(boardSquares[getSqrIdx(col + 1, row + dy * 1)]);
 	}
 
 	return squares;
+}
+
+bool Pawn::isValidMove()
+{
+	return 0;
 }
